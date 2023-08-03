2b2cbe183c385cd8c804f6b9af909cb0712fa3ce
Nkululeko Cyril Cele <nkululeko.cele@gmail.com>
2023-08-03 19:22:14 +0200

    Update templates, urls, and views

--- reservations/urls.py
+++ reservations/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", views.custom_login, name="login"),
     path("available_rooms/", views.available_rooms, name="available_rooms"),
-    path('guest_details/', views.guest_details, name='guest_details'),
+    path("guest_details/", views.guest_details, name="guest_details"),
+    path("reservation_details/", views.reservation_details,
+         name="reservation_details"),
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 
-from .models import Room, Guest
+from .models import Room, Guest, Reservation
 
 
 def register(request):
@@ -21,10 +21,15 @@ def custom_login(request):
 
 
 def available_rooms(request):
-    available_rooms = Room.objects.filter(is_available=True)
-    return render(request, "available_rooms.html", {"available_rooms": available_rooms})
+    rooms = Room.objects.filter(is_available=True)
+    return render(request, "available_rooms.html", {"rooms": rooms})
 
 
 def guest_details(request):
     guests = Guest.objects.all()
     return render(request, "guest_details.html", {"guests": guests})
+
+
+def reservation_details(request):
+    reservations = Reservation.objects.all()
+    return render(request, "reservation_details.html", {"reservations": reservations})
