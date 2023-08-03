48aca055fe73033aa48b59585b8938ae2c2ba07e
Nkululeko Cyril Cele <nkululeko.cele@gmail.com>
2023-08-03 18:58:05 +0200

    Update templates, urls, and views

--- reservations/urls.py
+++ reservations/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", views.custom_login, name="login"),
+    path("available_rooms/", views.available_rooms, name="available_rooms"),
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 
+from .models import Room
+
 
 def register(request):
     if request.method == "POST":
@@ -16,3 +18,8 @@ def register(request):
 
 def custom_login(request):
     return LoginView.as_view(template_name="registration/login.html")(request)
+
+
+def available_rooms(request):
+    available_rooms = Room.objects.filter(is_available=True)
+    return render(request, "available_rooms.html", {"available_rooms": available_rooms})
