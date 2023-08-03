afe758937ea4ef117014ccde7476bdbc9969bb67
Nkululeko Cyril Cele <nkululeko.cele@gmail.com>
2023-08-03 18:22:43 +0200

    Update settings, urls, and views, and add registration file

--- hotel_reservation/settings.py
+++ hotel_reservation/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = 'reservations/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
