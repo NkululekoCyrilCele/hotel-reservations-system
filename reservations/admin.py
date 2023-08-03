6c2d71ac3f0fea6154ea00dd23b96c82cdf2056b
Nkululeko Cyril Cele <nkululeko.cele@gmail.com>
2023-08-03 16:00:13 +0200

    Update admin, models, and migrations

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Room
 
-# Register your models here.
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("room_number", "room_type", "price", "is_available")
+    list_filter = ("room_type", "is_available")
+    search_fields = ("room_number", "description")
+
+
+admin.site.register(Room, RoomAdmin)
--- reservations/models.py
+++ reservations/models.py
@@ -1,1 +1,22 @@
 from django.db import models
+
+
+class Room(models.Model):
+    ROOM_TYPES = (
+        ("single", "Single"),
+        ("double", "Double"),
+        ("suite", "Suite"),
+    )
+
+    room_number = models.CharField(max_length=10, unique=True)
+    room_type = models.CharField(
+        max_length=10, choices=ROOM_TYPES, default="single")
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.room_number} - {self.room_type}"
+
+    class Meta:
+        ordering = ["room_number"]
