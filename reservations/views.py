ef83778722dcc2bfc59979b015cc7f486ea8521c
Nkululeko Cyril Cele <nkululeko.cele@gmail.com>
2023-08-03 18:37:45 +0200

    Update settings, templates, urls, and views

--- reservations/views.py
+++ reservations/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 
 
 def register(request):
@@ -11,3 +12,7 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "registration/register.html", {"form": form})
+
+
+def custom_login(request):
+    return LoginView.as_view(template_name="registration/login.html")(request)
